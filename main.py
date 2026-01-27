d8c6fa844f727609b65733243466c725c683b711
Sushant Bhandari <sushantbhandari238@gmail.com>
2026-01-26 22:03:26 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np 
+print(np.__version__)
+array= np.array([1,2,3,4])
+print(array)
+print(type(array))
+
+array = array *2
+print(array)
+
+# /Using python program
+a= [1,2,3,4]
+b=[]
+for num in a:
+    b.append(num * 2)
+
+print(b)
